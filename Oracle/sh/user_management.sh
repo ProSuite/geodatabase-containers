23f5683cee82819ae89c332d4467739c52b56910
paluchs <patrick.lu@bluewin.ch>
2025-01-14 09:47:15 +0100

    Added -s flag to sqlplus calls in user_management > surpresses sqlplus banners and removes unnecessary output for cleaner logs

--- Oracle/sh/user_management.sh
+++ Oracle/sh/user_management.sh
@@ -13,7 +13,7 @@ grant_role() {
     local role=$1
     local user=$2
 
-    sqlplus sys/"$ORACLE_PWD"@localhost:1521/"$ORACLE_PDB" as sysdba <<EOF
+    sqlplus -s sys/"$ORACLE_PWD"@localhost:1521/"$ORACLE_PDB" as sysdba <<EOF
 GRANT $role to $user;
 EXIT;
 EOF
@@ -24,7 +24,7 @@ create_user() {
     local password=$2
     local tablespace=$3
 
-    sqlplus sys/$ORACLE_PWD@localhost:1521/$ORACLE_PDB as sysdba "@/sql/user-management/create_user.sql" $user $password $tablespace
+    sqlplus -s sys/$ORACLE_PWD@localhost:1521/$ORACLE_PDB as sysdba "@/sql/user-management/create_user.sql" $user $password $tablespace
 }
 
 create_schema_owner() {
@@ -44,7 +44,7 @@ create_user_with_role() {
 
     create_user $user $password USERS
     grant_role $role $user
-    sqlplus sys/$ORACLE_PWD@localhost:1521/$ORACLE_PDB as sysdba <<EOF
+    sqlplus -s sys/$ORACLE_PWD@localhost:1521/$ORACLE_PDB as sysdba <<EOF
 GRANT UNLIMITED TABLESPACE TO $user;
 EXIT;
 EOF
