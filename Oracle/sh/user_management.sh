b251e8eb158bf14d81c22e4e2e56ee556a0d087b
paluchs <patrick.lu@bluewin.ch>
2025-01-21 16:13:01 +0100

    Extend user_management tools so that the db for which the function is executed can be passed as an argument (The name of the env var to use). Default is still the ORACLE_PDB env variable

--- Oracle/sh/user_management.sh
+++ Oracle/sh/user_management.sh
@@ -2,8 +2,10 @@
 
 create_role() {
     local role=$1
+    local pdb_var=${2:-ORACLE_PDB}  # Default to ORACLE_PDB if no second argument is passed
+    local pdb_value=${!pdb_var}     # Indirect expansion to get the value of the variable name passed
 
-    sqlplus -s sys/"$ORACLE_PWD"@localhost:1521/"$ORACLE_PDB" as sysdba <<EOF
+    sqlplus -s sys/"$ORACLE_PWD"@localhost:1521/"$pdb_value" as sysdba <<EOF
 CREATE ROLE $role;
 EXIT;
 EOF
@@ -12,8 +14,10 @@ EOF
 grant_role() {
     local role=$1
     local user=$2
+    local pdb_var=${3:-ORACLE_PDB}
+    local pdb_value=${!pdb_var}
 
-    sqlplus -s sys/"$ORACLE_PWD"@localhost:1521/"$ORACLE_PDB" as sysdba <<EOF
+    sqlplus -s sys/"$ORACLE_PWD"@localhost:1521/"$pdb_value" as sysdba <<EOF
 GRANT $role to $user;
 EXIT;
 EOF
@@ -22,36 +26,44 @@ EOF
 grant_rw_all_tables() {
     local role1=$1
     local role2=$2
+    local pdb_var=${3:-ORACLE_PDB}
+    local pdb_value=${!pdb_var}
 
-    sqlplus -s $SCHEMA_USER/$SCHEMA_PW@localhost:1521/$ORACLE_PDB "@/sql/user-management/grant_rw_all_tables.sql" $role1 $role2
+    sqlplus -s $SCHEMA_USER/$SCHEMA_PW@localhost:1521/$pdb_value "@/sql/user-management/grant_rw_all_tables.sql" $role1 $role2
 }
 
 create_user() {
     local user=$1
     local password=$2
     local tablespace=$3
+    local pdb_var=${4:-ORACLE_PDB}
+    local pdb_value=${!pdb_var}
 
-    sqlplus -s sys/$ORACLE_PWD@localhost:1521/$ORACLE_PDB as sysdba "@/sql/user-management/create_user.sql" $user $password $tablespace
+    sqlplus -s sys/$ORACLE_PWD@localhost:1521/$pdb_value as sysdba "@/sql/user-management/create_user.sql" $user $password $tablespace
 }
 
 create_schema_owner() {
     local user=$1
     local password=$2
     local tablespace=$3
+    local pdb_var=${4:-ORACLE_PDB}
+    local pdb_value=${!pdb_var}
 
-    create_user $user $password $tablespace
-    grant_role CONNECT $user
-    grant_role RESOURCE $user 
+    create_user $user $password $tablespace $pdb_var
+    grant_role CONNECT $user $pdb_var
+    grant_role RESOURCE $user $pdb_var
 }
 
 create_user_with_role() {
     local user=$1
     local password=$2
     local role=$3
+    local pdb_var=${4:-ORACLE_PDB}
+    local pdb_value=${!pdb_var}
 
-    create_user $user $password USERS
-    grant_role $role $user
-    sqlplus -s sys/$ORACLE_PWD@localhost:1521/$ORACLE_PDB as sysdba <<EOF
+    create_user $user $password USERS $pdb_var
+    grant_role $role $user $pdb_var
+    sqlplus -s sys/$ORACLE_PWD@localhost:1521/$pdb_value as sysdba <<EOF
 GRANT UNLIMITED TABLESPACE TO $user;
 EXIT;
 EOF
