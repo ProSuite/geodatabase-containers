74895c6f7f525a53d9898e56a451ede187663eec
paluchs <patrick.lu@bluewin.ch>
2025-02-03 10:43:20 +0100

    Create Tablespace on PDB and not in CBD

--- Oracle/sh/create_sde_tablespace.sh
+++ Oracle/sh/create_sde_tablespace.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sqlplus / as sysdba '@/sql/create_sde_tablespace.sql' $ORACLE_SID $ORACLE_PDB
+sqlplus sys/$ORACLE_PWD@localhost:1521/$ORACLE_PDB as sysdba '@/sql/create_sde_tablespace.sql' $ORACLE_SID $ORACLE_PDB
