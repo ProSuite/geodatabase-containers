0a82660f525e9dea5129aa5f2d257a8d9ebce257
linusrg <l.rueegg@dirageosystems.ch>
2025-04-03 10:04:48 +0200

    Turn into python function

--- helpers/arcpy/create_version.py
+++ helpers/arcpy/create_version.py
@@ -1,32 +1,50 @@
 import os
-import sys
 import traceback
 import arcpy
 
-sde_file_path = sys.argv[1]
-sde_file_name = sys.argv[2]
-version_name = sys.argv[3]
+def create_version(sde_file_path, sde_file_name, version_name):
+    """
+Creates a version in an ArcSDE geodatabase.
 
-ws = os.path.join(sde_file_path, sde_file_name)
+Parameters:
+sde_file_path (str): The path to the directory containing the SDE connection file.
+sde_file_name (str): The name of the SDE connection file.
+version_name (str): The name of the version to create.
 
-arcpy.env.overwriteOutput = True
+Returns:
+bool: True if the version is created successfully, False otherwise.
+    """
+    ws = os.path.join(sde_file_path, sde_file_name)
+    arcpy.env.overwriteOutput = True
 
-print('')
-print('Set workspace: {}'.format(ws))
-arcpy.env.workspace = ws
+    print('Set workspace: {}'.format(ws))
+    arcpy.env.workspace = ws
 
-print('Creating version {} ...'.format(version_name))
+    print('Creating version {} ...'.format(version_name))
 
-try:
-    arcpy.management.CreateVersion(ws, "SDE.DEFAULT", version_name, "PUBLIC")
-    
-    for i in range(arcpy.GetMessageCount()):
-        arcpy.AddReturnMessage(i)
-    print('')
+    try:
+        arcpy.management.CreateVersion(ws, "SDE.DEFAULT", version_name, "PUBLIC")
 
-except:
-    print(traceback.format_exc())
-    print('Press enter to exit')
-    input()
-    sys.exit(-1)
-    
+        for i in range(arcpy.GetMessageCount()):
+            arcpy.AddReturnMessage(i)
+        print('Version created successfully.')
+        return True
+
+    except Exception as e:
+        print('An error occurred while creating the version:')
+        print(traceback.format_exc())
+        return False
+
+
+if __name__ == "__main__":
+    # Example usage
+    sde_file_path = sys.argv[1]
+    sde_file_name = sys.argv[2]
+    version_name = sys.argv[3]
+
+
+    success = create_version(sde_file_path, sde_file_name, version_name)
+    if success:
+        print("Version created successfully.")
+    else:
+        print("Failed to create version.")
